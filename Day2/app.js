e088a94b5a031a03b9fc920d6d5b7758c223b2a7
Jilles Soeters <info@jilles.me>
2014-11-12 21:41:07 +0000

    Improved Day2 with a setController

--- Day2/app.js
+++ Day2/app.js
@@ -1,6 +1,18 @@
 var App = Ember.Application.create();
 
 App.Router.map(function () {
-  this.route('index', { path: '/' });
+  // this.route('index', { path: '/' }); -- this is default
   this.route('page', { path: '/page' });
+});
+
+App.IndexRoute = Ember.Route.extend({
+  setupController: function (controller) {
+    controller.set('title', 'Hoooomeee page!!!');
+  }
+});
+
+App.PageRoute = Ember.Route.extend({
+  setupController: function (controller) {
+    controller.set('title', '~ a page ~');
+  }
 });
